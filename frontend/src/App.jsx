269d02ee7048611ddb9ca8c62d2dd34d5283b9c6
johnnyblaze993 <john.alvarez.93@gmail.com>
2023-10-14 21:42:14 -0500

    first attempt to work from clone

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [digimons, setDigimons] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const fetchDigimons = () => {
+		axios.get("http://localhost:8080/digimon").then((response) => {
+			setDigimons(response.data);
+		});
+	};
+
+	return (
+		<>
+			<h2>Digi app</h2>
+			<button onClick={fetchDigimons}>Load Digimons</button>
+			<div style={{ display: "flex", flexWrap: "wrap" }}>
+				{digimons.map((digimon) => (
+					<div
+						key={digimon.id}
+						style={{
+							border: "1px solid black",
+							margin: "10px",
+							padding: "10px",
+							width: "200px",
+							borderRadius: "10px",
+						}}
+					>
+						<h3>{digimon.name}</h3>
+						<p>Type: {digimon.type}</p>
+						<p>Number: {digimon.digimonNumber}</p>
+					</div>
+				))}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
